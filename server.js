ebdbe6ce201b4090f80e83ed840cc098c823108a
yinlainghui <yinlianghui@hotoa.com>
2019-05-27 13:46:39 +0800

    fix 请求不到数据

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ server.Fiber(function () {
     server.Profile.run("Server startup", function () {
         server.loadServerBundles();
         try {
-
-            let objects = objectql.loadObjectFiles(path.resolve(__dirname, "./src"))
-            let apps = objectql.loadAppFiles(path.resolve(__dirname, "./src"))
-
-            objects.forEach(function(object){
-                Creator.Objects[object.name] = object
-            })
-
-            apps.forEach(function(app){
-                Creator.Apps[app._id] = app
-            })
-
+            var steedosSchema = objectql.getSteedosSchema()
+            steedosSchema.addDataSource('mattermost', {
+                driver: "postgres",
+                url: "postgresql://mmuser:mmuser_password@192.168.0.21:5432/mattermost",
+                objectFiles: [path.resolve(__dirname, "./src")],
+                appFiles: [path.resolve(__dirname, "./src")]
+            });
+            steedosSchema.getDataSource('mattermost').init();
         } catch (error) {
             console.log(error)
         }
